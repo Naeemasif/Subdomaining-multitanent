f6469dd89f2942abb6fe33c37da83a6556d29212
naeem asif <naeemgoraya@yahoo.com>
2014-01-13 17:17:35 +0500

    subdomaining complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3SubdomainDevise::Application.config.secret_token = '179c0f8ef895d0619415595a9b954ed1b3ec98fa691d9aca7ea73f7cbc25fc0f847a6b2e5664bbaece61308e241bacc0cef8b00447f3f2eb58772a44e2c79287'
